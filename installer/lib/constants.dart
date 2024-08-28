0b38d7ead1ca929428bbadd5481d32ffe5c25053
zt64 <31907977+zt64@users.noreply.github.com>
2024-08-28 15:50:36 -0400

    delete installer

--- installer/lib/constants.dart
+++
@@ -1,9 +0,0 @@
-/*
- * This file is part of Aliucord, an Android Discord client mod.
- * Copyright (c) 2023 Juby210 & Vendicated
- * Licensed under the Open Software License version 3.0
- */
-
-const backendHost = 'https://aliucord.com/';
-const defaultDexLocation = '/storage/emulated/0/Aliucord/Injector.dex';
-const supportServer = 'EsNDvBaHVU';
